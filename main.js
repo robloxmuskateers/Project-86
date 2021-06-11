d775e7a314aa0a5efc4afd1251fcc1ff41ba4361
robloxmuskateers <79553090+robloxmuskateers@users.noreply.github.com>
2021-06-11 16:38:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var canvas = new fabric.Canvas('myCanvas');
+block_image_width = 30;
+block_image_height = 30;
+
+var player_x = 10;
+var player_y = 10;
+
+var player_object = "";
+var block_image_object = "";
+
+function playerUpdate() {
+    fabric.Image.fromURL("player.png", function(Img) {
+        player_object = Img;
+
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set( {
+            top: player_y,
+            left: player_x
+        });
+        canvas.add(player_object);
+    });
+}
+
+function newImage(get_image) {
+    fabric.Image.fromURL(get_image, function(Img) {
+        block_image_object = get_image;
+
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set( {
+            top: player_y,
+            left: player_x
+        });
+        canvas.add(block_image_object);
+    });
+}
